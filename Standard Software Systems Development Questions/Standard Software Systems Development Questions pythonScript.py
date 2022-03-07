a377d9a2534080208e80763b9c148780dcb7f2d4
Mahmudul Alam <mahmudulalam81@gmail.com>
2022-03-07 09:03:33 -0500

    Add files via upload

---
+++ Standard Software Systems Development Questions/Standard Software Systems Development Questions pythonScript.py
@@ -0,0 +1,24 @@
+# -----------------------------------------------------------
+# Program to add two matrices using nested loop
+# (C) 2020 Mahmudul Alam
+# Released under Colorado State University-Global Campus
+# email mahmudul.alam@csuglobal.edu
+# -----------------------------------------------------------
+print('\n-----Standard Software Systems Development Questions-----\n')
+
+print('Why does it take so long to get software finished?')
+print('A software development process can be long and complex depending on the features. There is\nalways a gap between what customers wants and how developers understand it. It takes some\ntime to understand and come to a common ground which adds complexity to the whole process.\nDocumentation also plays a key role and adds to the complexity, it is important and critical\ndocumenting the whole development process for credibility and liability. Testing and verifying\neach build can be time consuming at times. ')
+print('\n')
+print('Why are development costs so high?')
+print('Software development requires highly skilled people and lots of people works together to develop\na product. Engineers can be costly due to the high salary, testing and unplanned changes adds extra\ncost to the development cost. A large team works on defining scops and writing/developing the product\nand finally testing the product takes lots of time and time is money. ')
+print('\n')
+print('Why cant we find all errors before we give the software to our customers?')
+print('Defining and understanding the project properly can be critical finishing the product on time.\nUnderstanding the reequipments wrong can led to errors. Also, if the peer review is not done right,\nwe can end up having lots of errors before giving the software to our customers. ')
+print('\n')
+print('Why do we spend so much time and effort maintaining existing programs?')
+print('A software is not only about building it, after a software built is completed it must be continuously\nupdated and upgrades will keep its value in the market. maintaining a software is a critical part of\nbuilding it, often it can require a large group of people to maintain it. We spend so much time and effort\nmaintaining existing programs because software needs updates for security reasons, to deal with changes in\nthe features and to deal with user data. ')
+print('\n')
+print('Why do we continue to have difficulty in measuring progress, as software is being developed and maintained?')
+print('A software is built for the first time without other relevant software that aligns exactly to this one. It\ncan be tough to measure the progress of a software if there is noting to compare it to. Errors, bugs ad defects\ncan always have impact on the software while developing it and even while maintaining it. ')
+print('\n')
+# ----------------------END-----------------------#
